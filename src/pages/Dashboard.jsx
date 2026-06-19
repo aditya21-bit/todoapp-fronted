b0ca159543cf2d0ff17faf34c6f9f79b6a0a237f
Aditya Gupta <guptaadityaa21@gmail.com>
2026-06-19 21:42:57 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import Navbar from "../components/Navbar";
 import "../styles/Dashboard.css";
 import { useState, useEffect } from "react";
-import API from "../api/api";
+import API from "../API/api";
 
 function Dashboard() {
 
